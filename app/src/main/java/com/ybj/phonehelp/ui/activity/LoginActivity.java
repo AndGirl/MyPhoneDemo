d262f0767ed37149a5213b6e6cb6322b41d6fa63
YBJ <15390264297@163.com>
2018-01-10 19:11:25 +0800

    原始动态授权

---
+++ app/src/main/java/com/ybj/phonehelp/ui/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.ybj.phonehelp.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ybj.phonehelp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
